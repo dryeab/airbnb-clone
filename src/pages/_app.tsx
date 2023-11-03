62d5dd68901db070f77d91ad260b815fbd38d18f
Yeabsira <dryeab@gmail.com>
2023-11-04 00:00:43 +0300

    Added favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -5,6 +6,15 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          sizes="76x76"
+          type="image/x-icon"
+          href="/assets/icons/favicon.ico"
+        ></link>
+        <title>Airbnb</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
